bd7af7ed531940085020727a78f6d804b62b9193
Alejandro Ruiz <alejandro@alejandroruizvarela.com>
2017-03-25 12:14:18 -0600

    [Second Commit]

---
+++ Lab1/Clima/View/ClimaView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace Clima.View
+{
+    public partial class ClimaView : ContentPage
+    {
+        ViewModels.ClimaViewModel vm;
+        public ClimaView()
+        {
+            InitializeComponent();
+            vm = new ViewModels.ClimaViewModel();
+            BindingContext = vm;
+        }
+    }
+}
